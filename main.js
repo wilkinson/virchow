b0674b2b12a26cd5ae335f3d284c7a795e40b8c6
Sean Wilkinson <sean@mathbiol.org>
2013-10-07 15:43:25 -0500

    Placeholder JS file

---
+++ main.js
@@ -0,0 +1,28 @@
+//- JavaScript source code
+
+//- main.js ~~
+//                                                      ~~ (c) SRW, 07 Oct 2013
+//                                                  ~~ last updated 07 Oct 2013
+
+(function () {
+    'use strict';
+
+ // Pragmas
+
+    /*jslint indent: 4, maxlen: 80 */
+
+ // Declarations
+
+ // Definitions
+
+ // Out-of-scope definitions
+
+ // Invocations
+
+ // That's all, folks!
+
+    return;
+
+}());
+
+//- vim:set syntax=javascript:

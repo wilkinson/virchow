4ae3e7c5db090f9855c2c609eaeef8cf3bee8460
Sean Wilkinson <sean@mathbiol.org>
2013-10-14 14:28:08 -0500

    Began implementing filters

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 //- main.js ~~
 //                                                      ~~ (c) SRW, 07 Oct 2013
-//                                                  ~~ last updated 13 Oct 2013
+//                                                  ~~ last updated 14 Oct 2013
 
 (function () {
     'use strict';
@@ -15,11 +15,45 @@
 
  // Declarations
 
-    var $, open, read_data_from_canvas, run, save;
+    var $, median_filter, open, read_data_from_canvas, run, save;
 
  // Definitions
 
     $ = window.jQuery;
+
+    median_filter = function () {
+     // This function applies a median mask to a color image by defining the
+     // median for RGBA values as the median of the L2 norms of a pixel and its
+     // neighborhood of radius 1 pixel (Chebyshev distance).
+        var f, padding;
+        f = function (i, j) {
+         // This function needs documentation.
+            var norm, x;
+            norm = function (p) {
+             // This function computes the L2 norm of the four element array
+             // representing a single pixel's RGBA values.
+                return Math.sqrt((p[0] * p[0]) + (p[1] * p[1]) +
+                    (p[2] * p[2]) + (p[3] * p[3]));
+            };
+            x = this;
+            return [
+                [x[i - 1][j - 1], norm(x[i - 1][j - 1])],
+                [x[i - 1][j], norm(x[i - 1][j])],
+                [x[i - 1][j + 1], norm(x[i - 1][j + 1])],
+                [x[i][j - 1], norm(x[i][j - 1])],
+                [x[i][j], norm(x[i][j])],
+                [x[i][j + 1], norm(x[i][j + 1])],
+                [x[i + 1][j - 1], norm(x[i + 1][j - 1])],
+                [x[i + 1][j], norm(x[i + 1][j])],
+                [x[i + 1][j + 1], norm(x[i + 1][j + 1])]
+            ].sort(function (a, b) {
+             // This function needs documentation.
+                return (a[1] < b[1]) ? -1 : 1;
+            })[4][0];
+        };
+        padding = 1;
+        return run(f, padding);
+    };
 
     open = function () {
      // This function needs documentation.
@@ -58,20 +92,50 @@
 
     read_data_from_canvas = function () {
      // This function needs documentation.
-        var canvas, ctx, data;
+        var canvas, ctx, i, index, j, m, n, x, y;
         canvas = document.getElementById('virchow-canvas');
         ctx = canvas.getContext('2d');
-        data = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        return data;
+        x = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        m = x.height;
+        n = x.width;
+        y = [];
+        for (i = 0; i < m; i += 1) {
+            y[i] = [];
+            for (j = 0; j < n; j += 1) {
+             // Find current pixel.
+                index = 4 * ((i * n) + j);
+                y[i][j] = [
+                    x.data[index],
+                    x.data[index + 1],
+                    x.data[index + 2],
+                    x.data[index + 3]
+                ];
+            }
+        }
+        return y;
     };
 
-    run = function (f) {
+    run = function (f, padding) {
      // This function, which is currently just a placeholder, will execute a
      // user-specified algorithm on the current contents of the HTML5 Canvas.
-     // It will divide the image into smaller pieces for distributed execution
-     // using QMachine (https://www.qmachine.org).
-        // ...
-        console.log(f);
+     // Eventually, it will divide the image into tiles to be distributed for
+     // parallel processing by QMachine (https://www.qmachine.org/).
+        if (padding === undefined) {
+         // Currently, we don't take this into account yet ...
+            padding = 0;
+        }
+        var col, m, n, row, x, y;
+        x = read_data_from_canvas();
+        m = x.length;
+        n = x[0].length;
+        y = [];
+        for (row = padding; row < (m - padding); row += 1) {
+            y[row] = [];
+            for (col = padding; col < (n - padding); col += 1) {
+                y[row][col] = f.call(x, row, col);
+            }
+        }
+        console.log(y);
         return;
     };
 
@@ -84,6 +148,7 @@
  // Out-of-scope definitions
 
     window.VIRCHOW = {
+        median_filter: median_filter,
         open: open,
         read_data_from_canvas: read_data_from_canvas,
         run: run,
